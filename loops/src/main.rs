d948b90f595131269034aaa19002be5e2cbc9d08
Kyle Rader <kyle@kylerader.ninja>
2016-12-25 18:26:14 -0500

    Add loops

---
+++ loops/src/main.rs
@@ -0,0 +1,21 @@
+/// # Loops in Rust
+
+fn main() {
+
+    /// ## Using `loop`
+    let mut x = 10;
+    loop {
+        x += x - 3;
+        println!("x is {}", x);
+        if x % 5 == 0 { break; }
+    }
+
+    /// Using `for`
+    for i in 0..5 {
+        println!("\"i\" is now {}", i);
+    }
+
+    for (i, val) in (1..11).enumerate() {
+        println!("{}: {}", i, val);
+    }
+}
