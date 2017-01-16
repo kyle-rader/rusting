32f5abf59340dcd2b45025596bc69364741e4a67
Kyle Rader <kyle@kylerader.ninja>
2017-01-15 23:05:34 -0800

    format print each element of a vector

--- rust-by-example/formatted-print/src/main.rs
+++ rust-by-example/formatted-print/src/main.rs
@@ -1,6 +1,4 @@
 
-
-
 fn main() {
     println!("Hello, formatted print!");
 
@@ -20,4 +18,13 @@ fn main() {
     let pi_5 = format!("{:.5}", pi);
 
     println!("Pi to 3 places: {} and to 5 places: {}", pi_3, pi_5);
+
+    // e, pi, & golden ratio
+    let special_nums = vec![2.71828, 3.14159, 1.61803];
+
+    // some iteration over a vector.
+    for special_num in &special_nums {
+        println!("This is a special number: {val:.3}", val=special_num);
+    }
+
 }
